6b1196ffbce2e6c563a4ec3bed95a1185f2dc176
ChangedenChan <1499450463@QQ.com>
2018-03-23 14:03:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OptimatVuejs基准库
+## 使用方式（Usage）
+### 安装（Install）
+``
+npm install optimat-vue-libs -save
+``
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Class from './lib/Class';
+import Context from './lib/Context';
+import BaseModule from './lib/BaseModule';
+import BrowserUtils from './utils/BrowserUtils';
+import ObjectSupport from './utils/ObjectSupport';
+
+module.exports = {
+    lib: {
+        Class,
+        Context,
+        BaseModule
+    },
+    utils: {
+        BrowserUtils, ObjectSupport
+    }
+};
